d365891f88f7e290b60f3370531e3e041051b1a1
MauroReyesCampos <mreyesc@gmail.com>
2024-01-11 14:44:14 -0500

    Create style.css & main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+menuEmail.addEventListener('click', () => {desktopMenu.classList.toggle('inactive')});
