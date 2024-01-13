3ddc2e2e3493c0f2ee4ef728434dfdadd209772e
MauroReyesCampos <mreyesc@gmail.com>
2024-01-13 15:20:38 -0500

    Mobile integration

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 menuEmail.addEventListener('click', () => {desktopMenu.classList.toggle('inactive')});
+menuHamIcon.addEventListener('click', () => {mobileMenu.classList.toggle('inactive')});
